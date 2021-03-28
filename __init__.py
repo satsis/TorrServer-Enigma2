d258e61d9c22e939bb0758c1466f3ac544d30e3c
Vasiliks <49211115+Vasiliks@users.noreply.github.com>
2021-03-28 09:54:59 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from os import environ
+from Components.Language import language
+from gettext import bindtextdomain, dgettext, gettext
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+
+def localeInit():
+ 	environ["LANGUAGE"] = language.getLanguage()[:2]
+	bindtextdomain("TorrServer", resolveFilename(SCOPE_PLUGINS, "Extensions/TorrServer/locale"))
+
+def _(txt):
+	t = dgettext("TorrServer", txt)
+	if t == txt:
+		t = gettext(txt)
+	return t
+ 
+
+localeInit()
+language.addCallback(localeInit)
+
